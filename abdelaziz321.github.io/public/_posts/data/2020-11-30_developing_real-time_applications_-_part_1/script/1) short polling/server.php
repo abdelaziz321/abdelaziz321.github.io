e95f76529a96a05097273c5e925097982653421c
abdelaziz321 <abdelazizmahmoud321@gmail.com>
2020-11-28 00:12:54 +0200

    Posts[1]: addiing short polling script

---
+++ abdelaziz321.github.io/public/_posts/data/2020-11-30_developing_real-time_applications_-_part_1/script/1) short polling/server.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * this is a mock to a function which perform a query and do some
+ * calculations to get the top seller today
+ * 
+ * @return array
+ */
+function getTopSellerToday() {
+    $sellers = ['Ahmed', 'Yasser', 'Ibrahim', 'Hussain', 'Hassan'];
+    return $sellers[array_rand($sellers)];
+}
+
+header('Content-type: application/json'); 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-Requested-With, content-type');
+
+echo json_encode([
+    'top_seller' => getTopSellerToday()
+]);
+
+# response example: {"top_seller":"Hassan"}
